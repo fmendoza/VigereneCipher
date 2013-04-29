b3d536a3668e7b49b25876cb6fc0361801ba1f3d
Fernando Mendoza <f.mendoza@outlook.com>
2013-04-29 13:46:41 -0500

    Comments deleted

--- VigenereCipher.java
+++ VigenereCipher.java
@@ -1,18 +1,3 @@
-/*
- * Usage (Method 1): 
- * System.out.print(new VigenereCipher(keyword, text))
- *  @param String keyword (Required)
- *  @param String text    (Required)
- *  @return the text encrypted
- * 
- * Usage (Method 2):
- * VigenereCipher obj = new VigenereCipher();
- * obj.setKeyword(keyword);
- * obj.setText(text); 
- * obj.encrypt();
- * System.out.print(obj.getCipherText())
- */
-
 public class VigenereCipher {
     
     private String keyword;
